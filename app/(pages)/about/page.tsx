3a491d47d95b4fddeed89b3929108e2930d06438
Daniel Büchele <daniel@buechele.cc>
2024-08-15 22:01:17 +0100

    about

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,6 +1,7 @@
 import Wrapper from "@/components/Wrapper/Wrapper";
 import styles from "./page.module.css";
 import PageHeader from "@/components/PageHeader/PageHeader";
+import Table, { Row } from "@/components/Table/Table";
 
 export default function About() {
   return (
@@ -16,6 +17,64 @@ export default function About() {
         <div className={styles.bento3}>3</div>
         <div className={styles.bento4}>4</div>
       </div>
+      <Table className={styles.table}>
+        <Row title={<h3>Our Story and Values</h3>}>
+          <p className={styles.lead}>
+            Our journey began in a small kitchen, where friends shared a passion
+            for cooking. We noticed that while we had access to a myriad of
+            spices and herbs, the tools we used to grind them were often
+            overlooked. We felt that something as fundamental as a pepper and
+            salt grinder deserved the same level of attention and craftsmanship
+            as any other kitchen tool.
+          </p>
+          <p className={styles.lead}>
+            Driven by this idea, we embarked on a mission to create the perfect
+            grinder—one that combines functionality, durability, and elegance.
+          </p>
+          <p className={styles.lead}>
+            We believe in creating products that stand the test of time,
+            meticulously crafting each grinder with high-quality materials and
+            precision engineering for exceptional performance and durability.
+            Committed to minimizing environmental impact, our refillable,
+            long-lasting grinders reduce waste and use sustainably sourced
+            materials and eco-friendly manufacturing processes. Designed to be
+            both functional and aesthetically pleasing, our grinders are easy to
+            use and maintain, adding a touch of elegance to any kitchen.
+          </p>
+          <p className={styles.lead}>
+            Our love for cooking drives us to create the best possible tools for
+            the kitchen. We believe that great food starts with the right
+            ingredients and the right tools, and we are dedicated to helping our
+            customers elevate their culinary creations.
+          </p>
+        </Row>
+
+        <Row title="Designed in">California</Row>
+        <Row title="Manufactured in">Germany</Row>
+        <Row title="Ecological">
+          <p>
+            Outstanding green credentials with durable, sustainable stainless
+            steel.
+          </p>
+        </Row>
+        <Row title="Economical">
+          <p>
+            High-quality and durable, made in Germany, and fully repairable.
+          </p>
+        </Row>
+        <Row title="Functional">
+          <p>
+            Easy to use with a unique grip, offering precise and excellent
+            grinding results.
+          </p>
+        </Row>
+        <Row title="Aesthetical">
+          <p>
+            Timeless, clean design with no fussy details, instantly
+            re-cognizable for its high quality.
+          </p>
+        </Row>
+      </Table>
     </Wrapper>
   );
 }
--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -6,15 +6,24 @@ const medium = localFont({
   src: "../../public/basiercircle-medium-webfont.woff2",
 });
 
-export function Row(props: { title: string; children: React.ReactNode }) {
+export function Row(props: {
+  title: string | React.ReactNode;
+  children: React.ReactNode;
+}) {
   return (
     <div className={styles.row}>
-      <h2 className={clsx(medium, styles.sectionTitle)}>{props.title}</h2>
+      <div className={clsx(medium, styles.sectionTitle)}>{props.title}</div>
       <div>{props.children}</div>
     </div>
   );
 }
 
-export default function Table({ children }: { children: React.ReactNode }) {
-  return <section className={styles.grid}>{children}</section>;
+export default function Table({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return <section className={clsx(styles.grid, className)}>{children}</section>;
 }
