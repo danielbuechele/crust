d3605b58b09f3bdc0aabddc237f933f68e74bf33
Daniel Büchele <daniel@buechele.cc>
2024-09-28 17:13:48 +0100

    guide

---
+++ app/(pages)/guide/page.tsx
@@ -0,0 +1,14 @@
+import PageHeader from "@/components/PageHeader/PageHeader";
+import Link from "next/link";
+
+export default function Guide() {
+  return (
+    <div>
+      <PageHeader
+        title="Pepper and Salt
+        Buying Guide"
+        lead="Our guide to pepper and salt types and flavors to elevate your cooking experience."
+      />
+    </div>
+  );
+}
